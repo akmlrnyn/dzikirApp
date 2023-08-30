2132f1ddfd2331fef14cd486566cd5d8f3d56aed
akmalranyan <akmalranyan@gmail.com>
2023-08-30 09:05:12 +0700

    made models and layouts, adapter and import data locally

--- app/src/main/java/com/nyan/doadzikirapp/presentation/pagipetang/PetangActivity.kt
+++ app/src/main/java/com/nyan/doadzikirapp/presentation/pagipetang/PetangActivity.kt
@@ -2,15 +2,30 @@ package com.nyan.doadzikirapp.presentation.pagipetang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nyan.doadzikirapp.DoaDzikirAdapter
 import com.nyan.doadzikirapp.R
+import com.nyan.doadzikirapp.databinding.ActivityPetangBinding
+import com.nyan.doadzikirapp.model.DataDoaDzikir
+import com.nyan.doadzikirapp.model.DoaDzikirItem
 
 class PetangActivity : AppCompatActivity() {
+    private var _binding: ActivityPetangBinding? = null
+    private val binding get() = _binding as ActivityPetangBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //show page navigation button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        setContentView(R.layout.activity_petang)
+        _binding = ActivityPetangBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myAdapter = DoaDzikirAdapter()
+        myAdapter.setData(DataDoaDzikir.listDzikirPetang())
+        binding.rvDzikirPetang.adapter = myAdapter
+        binding.rvDzikirPetang.layoutManager = LinearLayoutManager(this)
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
